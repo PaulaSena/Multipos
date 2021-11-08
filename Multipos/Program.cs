055e7f78e3448ae5c2af1518d890ab8ce3b41078
PaulaSena <senasilvaanapaula@gmail.com>
2021-11-08 16:20:40 -0300

    Atividade Multiplos

--- Multipos/Program.cs
+++ Multipos/Program.cs
@@ -10,6 +10,125 @@ namespace Multipos
     {
         static void Main(string[] args)
         {
+            /*
+             *  - A aplicação deve solicitar um número ao usuário.
+             *  - Em seguida a aplicação irá somar todos os números naturais de 0 até o número informado que sejam múltiplos de 3 ou 5.
+             *  - No final, deverá ser exibida a soma desses números ao usuário.
+             * 
+             */
+
+            // =>  DESCOMENTAR CADA BLOCO ANTES DE EXECULTAR
+
+            #region  Valores multiplos de 3 
+
+            //int n;
+            //Console.WriteLine("Digite um numero:");
+            //n = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    if (i % 3 == 0)
+            //    {
+            //        Console.Write("{0} | ", i);
+
+            //    }
+
+            //}
+            //Console.ReadKey();
+
+            #endregion
+
+
+            #region  Valores multiplos de 5 
+
+            //int n;
+            //Console.WriteLine("Digite um numero:");
+            //n = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    if (i % 5 == 0)
+            //    {
+            //        Console.Write("{0} | ", i);
+
+            //    }
+
+            //}
+            //Console.ReadKey();
+
+            #endregion
+
+
+            #region  SOMA DE números Naturais.
+           
+            //int n;
+            //int result;
+            //Console.WriteLine("Digite um numero");
+            //n = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 0; i <= n; i++)
+            //{
+            //    if (i % 3 == 0)
+            //    {
+
+            //        //Console.Write("{0} | ", i + i);
+
+            //    }
+
+            //    Console.WriteLine("Soma : " + i + "+" + n + "=" + (result = n + i));
+            //}
+            ////Console.WriteLine("soma = " + soma + " + " + soma + " = " + result);
+
+            //Console.ReadKey();
+          
+            #endregion
+
+
+            #region  SOMA DE números naturais de 0 até o número informado que sejam múltiplos de 3 ou 5.
+            //int n;
+            //int result;
+            //Console.WriteLine("Digite um numero");
+            //n = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 0; i <= n; i++)
+            //{
+            //    if (i % 3 == 0)
+            //    {
+
+            //        Console.Write("{0} | ", i);
+
+            //    }
+            //    Console.WriteLine("Soma : " + i + "+" + n + "=" + (result = n + i));
+
+            //}
+
+            //Console.ReadKey();
+
+
+            #endregion
+
+
+            #region Soma Multiplos
+
+            //int n;
+            //int soma = 0;
+            //int result = 0;
+            //Console.WriteLine("Digite um numero");
+            //n = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    if (i % 3 == 0)
+            //    {
+            //        soma = i;
+            //        result = soma + soma;
+            //        Console.Write("{0} | ", i);
+
+            //    }
+            //    Console.WriteLine("soma = " + soma + " + " + soma + " = " + result);
+            //}
+            //// Console.WriteLine("soma = " + soma + " + " + soma + " = " + result);
+
+            //Console.ReadKey();
+
+            #endregion
+
         }
+
     }
 }
---
+++ README.md
@@ -0,0 +1,56 @@
+# Múltiplos C#
+
+-Valores múltiplos de 3
+
+-Valores múltiplos de 5
+
+-Soma de números naturais
+
+-Soma de números naturais de 0 até o número informado que —sejam múltiplos
+
+-Soma de Múltiplos
+
+![img](img/img1.png)
+
+# Valores múltiplos de 3
+
+```
+        #region  Valores multiplos de 3
+				int n;
+        Console.WriteLine("Digite um numero");
+        n = Convert.ToInt32(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 3 == 0)
+            {
+                Console.Write("{0} | ", i);
+
+            }
+
+        }
+        Console.ReadKey();
+
+        #endregion
+```
+
+![img](img/img2.png)
+
+# Valores múltiplos de 5
+
+![img](img/img3.png)
+
+![img](img/img4.png)
+
+# Soma de números naturais
+
+![img](img/img5.png)
+
+# Soma de números naturais de 0 até o número informado que sejam múltiplos
+
+![img](img/img6.png)
+
+# Soma de Múltiplos
+
+![img](img/img7.png)
+
+PaulaSena
